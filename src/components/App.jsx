deab77c62fd44a96da43e81112f894d36af8e5db
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2023-10-21 00:59:02 +0300

    change12

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
 
@@ -6,7 +6,7 @@ import ImageGallery from './ImageGallery';
 import Searchbar from './Searchbar';
 import Button from './Button';
 import Loader from './Loader';
-import { getImages, APIURL, KEY } from './api';
+import { getImages } from './api';
 
 class App extends Component {
   state = {
@@ -72,4 +72,76 @@ class App extends Component {
     );
   }
 }
+export default App;*/
+import React, { Component } from 'react';
+
+import ImageGallery from './ImageGallery';
+import Searchbar from './Searchbar';
+import Button from './Button';
+import Loader from './Loader';
+import { getImages } from './api';
+
+class App extends Component {
+  state = {
+    imageName: '',
+    images: [],
+    page: 1,
+    loading: false,
+    error: null,
+    perPage: 12,
+  };
+
+  getImages = () => {
+    this.setState({ loading: true });
+    getImages(this.state.imageName, this.state.page, this.state.perPage)
+      .then(res =>
+        this.setState(({ images }) => ({
+          images: [...images, ...res.data.hits],
+        }))
+      )
+      .catch(error => this.setState({ error }))
+      .finally(() => this.setState({ loading: false }));
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.imageName !== this.state.imageName) {
+      this.getImages();
+    }
+  }
+
+  loadMore = () => {
+    this.setState(
+      ({ page }) => ({
+        page: page + 1,
+      }),
+      this.getImages
+    );
+  };
+
+  handleChangeName = imageName => {
+    this.setState({ imageName, page: 1, images: [] }, this.getImages);
+  };
+
+  render() {
+    const { loading, images, perPage } = this.state;
+    return (
+      <>
+        <Searchbar onSubmit={this.handleChangeName} />
+
+        <ImageGallery images={images} />
+
+        {images.length === 0 && !loading && (
+          <h2 style={{ textAlign: 'center' }}>No images for showing</h2>
+        )}
+
+        {loading && <Loader />}
+
+        {images.length >= perPage &&
+          images.length % perPage === 0 &&
+          !loading && <Button onClick={this.loadMore} />}
+      </>
+    );
+  }
+}
+
 export default App;
