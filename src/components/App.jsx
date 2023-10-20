432f3097300ff25f58e92b9dd54fa587d9a6eac8
NPC-Roman <128088564+NPC-Roman@users.noreply.github.com>
2023-10-21 00:33:50 +0300

    Change10

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+/*import React, { Component } from 'react';
 import axios from 'axios';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css';
@@ -34,7 +34,7 @@ class App extends Component {
       .catch(error => this.setState({ error }))
       .finally(() => this.setState({ loading: false }));
   };
-  /*
+  */ /*
   componentDidUpdate(_, prevState) {
     if (
       prevState.images !== this.props.images &&
@@ -43,7 +43,7 @@ class App extends Component {
       toast('No images');
     }
   }
-*/
+*/ /*
   componentDidUpdate(_, prevState) {
     if (
       prevState.images !== this.state.images &&
@@ -64,6 +64,165 @@ class App extends Component {
 
   handleChangeName = imageName => {
     this.setState({ imageName, page: 1, images: [] }, this.getImages);
+  };
+
+  render() {
+    const { loading, images, perPage } = this.state;
+    return (
+      <>
+        <Searchbar onSubmit={this.handleChangeName} />
+
+        <ImageGallery images={images} />
+
+        {images.length === 0 && !loading && (
+          <h2 style={{ textAlign: 'center' }}>No images for showing</h2>
+        )}
+
+        {loading && <Loader />}
+
+        {images.length >= perPage &&
+          images.length % perPage === 0 &&
+          !loading && <Button onClick={this.loadMore} />}
+
+        <ToastContainer />
+      </>
+    );
+  }
+}
+
+export default App;*/ /*
+import React, { Component } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
+import ImageGallery from './ImageGallery';
+import Searchbar from './Searchbar';
+import Button from './Button';
+import Loader from './Loader';
+import { getImages, APIURL, KEY } from './api'; // Імпортуємо функцію та константи з api.js
+
+class App extends Component {
+  state = {
+    imageName: '',
+    images: [],
+    page: 1,
+    loading: false,
+    error: null,
+    perPage: 12,
+  };
+
+  getImages = () => {
+    this.setState({ loading: true });
+    getImages(this.state.imageName, this.state.page, this.state.perPage)
+      .then(res =>
+        this.setState(({ images }) => ({
+          images: [...images, ...res.data.hits],
+        }))
+      )
+      .catch(error => this.setState({ error }))
+      .finally(() => this.setState({ loading: false }));
+  };
+
+  componentDidUpdate(_, prevState) {
+    if (
+      prevState.images !== this.state.images &&
+      this.state.images.totalHits === 0
+    ) {
+      toast('No images');
+    }
+  }
+
+  loadMore = () => {
+    this.setState(
+      ({ page }) => ({
+        page: page + 1,
+      }),
+      this.getImages
+    );
+  };
+
+  handleChangeName = imageName => {
+    this.setState({ imageName, page: 1, images: [] }, this.getImages);
+  };
+
+  render() {
+    const { loading, images, perPage } = this.state;
+    return (
+      <>
+        <Searchbar onSubmit={this.handleChangeName} />
+
+        <ImageGallery images={images} />
+
+        {images.length === 0 && !loading && (
+          <h2 style={{ textAlign: 'center' }}>No images for showing</h2>
+        )}
+
+        {loading && <Loader />}
+
+        {images.length >= perPage &&
+          images.length % perPage === 0 &&
+          !loading && <Button onClick={this.loadMore} />}
+
+        <ToastContainer />
+      </>
+    );
+  }
+}
+
+export default App;*/
+import React, { Component } from 'react';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
+
+import ImageGallery from './ImageGallery';
+import Searchbar from './Searchbar';
+import Button from './Button';
+import Loader from './Loader';
+import { getImages } from './api';
+
+class App extends Component {
+  state = {
+    imageName: '',
+    images: [],
+    page: 1,
+    loading: false,
+    error: null,
+    perPage: 12,
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.imageName !== this.state.imageName) {
+      this.getImages();
+    }
+
+    if (this.state.images.length > 0 && this.state.images.totalHits === 0) {
+      toast('No images');
+    }
+  }
+
+  getImages = () => {
+    this.setState({ loading: true });
+    getImages(this.state.imageName, this.state.page, this.state.perPage)
+      .then(res =>
+        this.setState(({ images }) => ({
+          images: [...images, ...res.data.hits],
+        }))
+      )
+      .catch(error => this.setState({ error }))
+      .finally(() => this.setState({ loading: false }));
+  };
+
+  loadMore = () => {
+    this.setState(
+      ({ page }) => ({
+        page: page + 1,
+      }),
+      this.getImages
+    );
+  };
+
+  handleChangeName = imageName => {
+    this.setState({ imageName, page: 1, images: [] });
   };
 
   render() {
